bc636d3d8d7b601be1e656aeaaf1c0befddd562e
tazeenpatel123 <150174275+tazeenpatel123@users.noreply.github.com>
2023-12-21 20:28:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for (let i = 0; i < str.length; i++) {
+		let char=str.charAt(i);
+		if (str.indexOf(char)===str.lastIndexOf(char)) {
+			return char;
+			
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
